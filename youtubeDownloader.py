6d7b0d0cf53eb96c049af43486a6384819fd4b9f
aiaoxd <40092193@qq.com>
2024-12-24 01:56:56 +0800

    Update youtubeDownloader.py

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -76,7 +76,16 @@ def download_video(formats, selected_quality_index, url):
     print(f"正在下载 {quality_text} 格式...")
 
     # 使用 youtube-dl 进行下载操作，确保选中合适的格式。
-    ydl_opts = {'format': selected_format['format_id'], 'progress_hooks': [show_progress]}
+    # ydl_opts = {'format': selected_format['format_id'], 'progress_hooks': [show_progress]}
+    ydl_opts = {
+            'format': 'bestvideo+bestaudio/best',  # 下载最佳视频和音频并合并
+            'postprocessors': [{  # 使用后处理器将音频和视频合并成 MP4 格式
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4',  # 强制转换为 mp4 格式
+            }],
+            'progress_hooks': [self.show_progress],  # 显示进度
+            'quiet': False,  # 显示更多信息
+        }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
